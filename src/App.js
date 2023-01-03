1d099553ccff088f6f745754556b5b43b6d125cd
PravaatChhetri <pravaatchhetri66@gmail.com>
2023-01-03 14:36:19 +0600

    latest test commmit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import AlarmsAndEvents from './pages/AlarmsAndEvents';
-import Calender from './pages/Calender';
+import Calendar from './pages/Calendar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/alarms-and-events" element={<AlarmsAndEvents/>}/>
-          <Route path="/calender" element={<Calender/>}/>
+          <Route path="/calender" element={<Calendar/>}/>
           <Route path="*" element={<h1>You are Lost my dear</h1>}/>
         </Routes>
         <Footer />
