dcccd5b2f34199ac9e67da97e1ffd3e9c61a13ad
PrernaChhetri <110452388+PrernaChhetri@users.noreply.github.com>
2023-01-11 13:02:55 +0600

    Protected Routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 // import SignupPage from './pages/Signup';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProtectedRoutes from './utils/ProtectedRoutes';
 
 import DashHome from './pages/DashHome';
 import SCADADisp from './pages/SCADADisp';
@@ -12,8 +13,6 @@ import EventLog from './pages/EventLog';
 import WaterFlowDisplay from './pages/WaterFlowDisplay';
 import ResponsiveDrawer from './components/DashboardDrawer';
 import { useState } from 'react';
-
-
 
 function App() {
   
@@ -28,7 +27,7 @@ function App() {
   return (
     <>
       <Router>
-      {isLoggedIn ? (
+        {isLoggedIn ? (
         <button onClick={logOut}>Logout</button>
         ) : (
         <button onClick={logIn}>Login</button>
@@ -36,22 +35,19 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
+          <Route path="*" element={<h1 className='text-white'>You are Lost my dear</h1>}/>
 
-            <Route path="/DashBoard" element={<DashHome/>}/>
-            <Route path="/DashBoard/SCADA-Display" element={<SCADADisp/>}/>
-            <Route path="/DashBoard/Water-Flow-Display" element={<WaterFlowDisplay/>}/>
-            <Route path="/DashBoard/Valve-Controller" element={<ValveController/>}/>
-            <Route path="/DashBoard/Alarms-and-Events" element={<AlarmEvents/>}/>
-            <Route path="/DashBoard/Event-Log" element={<EventLog/>}/>
-
-          <Route path="/DashBoard/Log-Out" element={<ResponsiveDrawer box={<h1 className="text-center">Log Out</h1>}/>}/>
-          
-                
-          <Route path="*" element={<h1>You are Lost my dear</h1>}/>
+          <Route  element={<ProtectedRoutes/>}>
+          <Route path="/Dashboard" element={<DashHome/>}/>
+          <Route path="/Dashboard/SCADA-Display" element={<SCADADisp/>}/>
+          <Route path="/Dashboard/Water-Flow-Display" element={<WaterFlowDisplay/>}/>
+          <Route path="/Dashboard/Valve-Controller" element={<ValveController/>}/>
+          <Route path="/Dashboard/Alarms-and-Events" element={<AlarmEvents/>}/>
+          <Route path="/Dashboard/Event-Log" element={<EventLog/>}/>
+          <Route path="/Dashboard/Log-Out" element={<ResponsiveDrawer box={<h1 className="text-center">Log Out</h1>}/>}/>
+          </Route>
         </Routes>
-        
       </Router>
-      
     </>
   );
 }
